058ec08a9abf6c459f61c0e1a6294be2cf03a462
edwinndungu <edwin.ndungu2@student.moringaschool.com>
2025-03-10 20:06:48 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,20 @@
+function receivesAFunction(callback) {
+  callback();   
+    
+}
+
+function returnsANamedFunction() {
+  return function namedFunction() {
+      
+  }
+}
+
+function returnsAnAnonymousFunction() {
+  return function() {
+      
+  }
+}
+
+module.exports = {  receivesAFunction, returnsANamedFunction, returnsAnAnonymousFunction }
+
+
